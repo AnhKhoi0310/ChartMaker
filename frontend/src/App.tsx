31ed3f701ddab1ed4e247cbfb27f2443a1d74942
unknown <khoianh310@gmail.com>
2026-03-26 10:26:28 -0400

    change .env and get env

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -52,7 +52,7 @@ function App() {
       
       setMessages(prev => [...prev, { sender: "bot", text: "Generating chart... (this may take a few seconds)" }]);
       
-      const res = await axios.post("http://localhost:5000/chat", formData, {
+      const res = await axios.post("https://chartmaker-b020bfa117d7.herokuapp.com/chat", formData, {
         headers: { "Content-Type": "multipart/form-data" },
         timeout: 120000 // 120 seconds timeout
       });
